f01f31dcc21abe848d704d82af31a803c5e65466
Oscar Ho <oscar.ho0210@gmail.com>
2021-11-09 14:05:42 +0800

    initial commit

---
+++ src/com/tom/Stars.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Stars {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 20; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
