05751aec113751134faf4f7cfa1dba5d48b2a435
Oscar Ho <oscar.ho0210@gmail.com>
2021-11-09 15:18:11 +0800

    initial commit

---
+++ src/com/tom/StarX.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class StarX {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 20; j++){
+                if (2 * (9-i) == j ||  2 * i == j){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
