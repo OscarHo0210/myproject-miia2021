cb9e890b7f7003a2d6e615515e741ad0bdaa59f4
OscarHo0210 <oscar.ho0210@gmail.com>
2021-11-23 15:27:28 +0800

    Practice at class.

--- src/com/tom/VendingMachine.java
+++ src/com/tom/VendingMachine.java
@@ -6,29 +6,52 @@ public class VendingMachine {
     public static void main(String[] args) {
         int total = 0;
         boolean end = false;
+        int aDrinkPrice = 15;
+        int bDrinkPrice = 25;
         Scanner scanner = new Scanner(System.in);
 
         while (!end){
             System.out.println("Please put your coin(1/5/10) total: " + total);
-            int coin = scanner.nextInt();
+            String s = scanner.next();
 
-            switch (coin){
-                case 1:
-                    total = total + 1;
-                    break;
-                case 5:
-                    total = total + 5;
-                    break;
-                case  10:
-                    total = total + 10;
-                    break;
-                case 0:
-                    end = true;
-                    System.out.println("end");
-                    break;
-                default:
-                    break;
+            try {
+                int coin = Integer.parseInt(s);
+                switch (coin){
+                    case 1:
+                    case 5:
+                    case 10:
+                        total = total + coin;
+                        break;
+                    case 0:
+                        end = true;
+                        System.out.println("end");
+                        break;
+                    default:
+                        break;
+                }
+
+            }catch (NumberFormatException e){
+                System.out.println("Wrong");
+                if (s.equals("a")){
+                    System.out.println("Drink A");
+                }
+                if (total >= aDrinkPrice){
+                    total = total - aDrinkPrice;
+                    System.out.println("DON");
+                }else {
+                    System.out.println("BEEP");
+                }
+
+                break;
             }
         }
+                /* System.out.println("Choose drink a or b: ");
+                 String drink = scanner.next();
+                 switch (drink){
+                     String a;
+                     String b;
+                     case a:
+                 }*/
+
     }
 }
