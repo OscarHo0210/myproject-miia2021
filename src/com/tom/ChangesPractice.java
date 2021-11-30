7516355240d29bd580b38de252beacb3a0c5246e
OscarHo0210 <oscar.ho0210@gmail.com>
2021-11-30 13:38:27 +0800

    Pracetice in class(11/30).

---
+++ src/com/tom/ChangesPractice.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class ChangesPractice {
+    public static void main(String[] args) {
+        int i = 38;
+        System.out.println("10: " + (i/10));
+        System.out.println("5:  " + (i-(i/10)*10-i%5)/5);
+        System.out.println("1:  " + (i % 5));
+    }
+}
