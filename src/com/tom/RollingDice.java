c35032519caa93deba06b7c2993c324f944d65c2
Oscar Ho <oscar.ho0210@gmail.com>
2021-12-06 23:06:30 +0800

    Practice at home(1206).

--- src/com/tom/RollingDice.java
+++ src/com/tom/RollingDice.java
@@ -1,17 +1,17 @@
 package com.tom;
 
-
 import java.util.Random;
 
 public class RollingDice {
     public static void main(String[] args) {
         Random random = new Random();
         int[] dices = new int[6];
-        for (int i = 1; i < 11; i++){
-            int number = random.nextInt(6)+1;
-            System.out.println("(" + i + ")" + number);
 
-            switch (number){
+        for (int i =1; i<11; i++){
+            int num = random.nextInt(6)+1;
+            System.out.println(num);
+
+            switch (num){
                 case 1:
                     dices[0]++;
                     break;
@@ -30,16 +30,10 @@ public class RollingDice {
                 case 6:
                     dices[5]++;
                     break;
-                default:
-                    System.out.println("end");
-                    System.out.println(number);
-
             }
-
         }
-        for(int j = 0; j<dices.length;j++){
+        for (int j =0; j< dices.length; j++){
             System.out.println(j+1 + " -> " + dices[j] + " times");
         }
-
     }
 }
