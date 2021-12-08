10cdb2fa56e2a20690f2eab48b8500015858dea6
Oscar Ho <oscar.ho0210@gmail.com>
2021-12-08 16:28:27 +0800

    Practice at home(1208).

--- src/com/tom/RollingDice.java
+++ src/com/tom/RollingDice.java
@@ -10,7 +10,6 @@ public class RollingDice {
         for (int i =1; i<11; i++){
             int num = random.nextInt(6)+1;
             System.out.println(num);
-
             switch (num){
                 case 1:
                     dices[0]++;
@@ -30,12 +29,11 @@ public class RollingDice {
                 case 6:
                     dices[5]++;
                     break;
-                default:
-                    System.out.println("end");
             }
         }
-        for (int j =0; j< dices.length; j++){
+        for (int j=0; j< dices.length; j++){
             System.out.println(j+1 + " -> " + dices[j] + " times");
         }
     }
+
 }
