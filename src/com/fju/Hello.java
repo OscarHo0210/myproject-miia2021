b5603120e91c147139b3993a5853cbb8339c9e49
OscarHo0210 <oscar.ho0210@gmail.com>
2022-01-04 14:46:41 +0800

    Practice in class(0104)

--- src/com/fju/Hello.java
+++ src/com/fju/Hello.java
@@ -2,6 +2,9 @@ package com.fju;
 
 public class Hello {
     public static void main(String[] args) {
+        //類別是什麼 Hello String System
+        //變數 args
+        //方法println()
         System.out.println("Hello");
 
     }
